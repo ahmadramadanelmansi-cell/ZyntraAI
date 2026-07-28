dc6369bfc65ac396750e97343523e5674fb2df46
Ahmad Ramadan <ahmadramadanelmansi@gmail.com>
2026-07-28 13:46:42 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,49 +1,10 @@
+
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+  const response = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models?key=${process.env.OPENAI_API_KEY}`
+  );
 
-  try {
-    const { message } = req.body;
+  const data = await response.json();
 
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.OPENAI_API_KEY}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          contents: [
-            {
-              parts: [
-                {
-                  text: message
-                }
-              ]
-            }
-          ]
-        })
-      }
-    );
-
-    const data = await response.json();
-
-    if (data.error) {
-      return res.status(200).json({
-        reply: JSON.stringify(data.error)
-      });
-    }
-
-    return res.status(200).json({
-      reply:
-        data.candidates?.[0]?.content?.parts?.[0]?.text ||
-        "No response from Gemini"
-    });
-
-  } catch (error) {
-    return res.status(500).json({
-      reply: error.message
-    });
-  }
+  res.status(200).json(data);
 }
